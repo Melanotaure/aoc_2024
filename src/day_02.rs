13a884569dc8a7378f9f70e0877d62e193f46a14
Melanotaure <luc.gaborit@gmail.com>
2024-12-03 23:07:42 +0100

    day02 part1

--- src/day_02.rs
+++ src/day_02.rs
@@ -1,25 +1,42 @@
+#[derive(PartialEq)]
+enum Tendance {
+    Increasing,
+    Decreasing,
+}
 
 #[aoc(day2, part1)]
 fn part1(input: &str) -> u32 {
     let lines = input.lines();
-    let mut safe_count = 0;
+    let mut safe_count: u32 = 0;
 
-    for line in lines {
+'outer: for line in lines {
         let report: Vec<&str> = line.split_whitespace().collect();
 
         let max = report.len();
-        let mut current_diff = report[1].parse::<i8>().unwrap() - report[0].parse::<i8>().unwrap();
-        if current_diff == 0 || current_diff > 3 {
+        let state: Tendance;
+        let diff = report[1].parse::<i8>().unwrap() - report[0].parse::<i8>().unwrap();
+        if diff < 0 && diff > -4 {
+            state = Tendance::Decreasing;
+        } else if diff > 0 && diff < 4 {
+            state = Tendance::Increasing;
+        } else {
             continue;
         }
-
-        for i in 1..(max-1) {
-            let diff = report[i+1].parse::<i8>().unwrap() - report[i].parse::<i8>().unwrap();
-            if diff == 0 {
-                break;
+        for i in 1..(max - 1) {
+            let diff = report[i + 1].parse::<i8>().unwrap() - report[i].parse::<i8>().unwrap();
+            let current_state: Tendance;
+            if diff < 0 && diff > -4 {
+                current_state = Tendance::Decreasing;
+            } else if diff > 0 && diff < 4 {
+                current_state = Tendance::Increasing;
+            } else {
+                continue 'outer;
             }
-
+            if current_state != state {
+                continue 'outer;
+            }
         }
+        safe_count += 1;
     }
-    0
+    safe_count
 }
