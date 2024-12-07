ed0d8694a36004f7d6bc2b9cdd5ade9ede8b16bd
Melanotaure <luc.gaborit@gmail.com>
2024-12-07 16:06:23 +0100

    day06 part2

--- src/day_06.rs
+++ src/day_06.rs
@@ -2,7 +2,7 @@ enum Obstacle {
     Nothing,
     Object,
     Visited,
-    Exit,
+    Exit(bool),
 }
 
 #[derive(Debug)]
@@ -56,7 +56,7 @@ fn part1(input: &str) -> u32 {
     let mut move_count = 1;
     loop {
         match test_forward(&map, &guard) {
-            Obstacle::Exit => break,
+            Obstacle::Exit(_) => break,
             Obstacle::Nothing => {
                 map[guard.y as usize][guard.x as usize] = b'X';
                 guard.move_forward();
@@ -77,7 +77,7 @@ fn test_forward(map: &Vec<Vec<u8>>, bot: &Bot) -> Obstacle {
     let fx = bot.x + bot.dx;
     let fy = bot.y - bot.dy;
     if fy < 0 || fx < 0 || fy >= map.len() as i32 || fx >= map[fy as usize].len() as i32 {
-        return Obstacle::Exit;
+        return Obstacle::Exit(true);
     }
     if map[fy as usize][fx as usize] == b'.' {
         return Obstacle::Nothing;
@@ -88,5 +88,90 @@ fn test_forward(map: &Vec<Vec<u8>>, bot: &Bot) -> Obstacle {
     if map[fy as usize][fx as usize] == b'X' {
         return Obstacle::Visited;
     }
-    Obstacle::Exit
+    Obstacle::Exit(false)
+}
+
+#[aoc(day6, part2)]
+fn part2(input: &str) -> u32 {
+    let lines = input.lines();
+    let mut map: Vec<Vec<(u8, u8)>> = Vec::new();
+
+    for line in lines {
+        let mut l: Vec<(u8, u8)> = Vec::new();
+        for ch in line.chars() {
+            l.push((ch as u8, 0));
+        }
+        map.push(l);
+    }
+
+    // let's find the starting point
+    let mut start = (0, 0);
+    'out: for (j, line) in map.iter().enumerate() {
+        for (i, elm) in line.iter().enumerate() {
+            if elm.0 == b'^' {
+                start = (i, j);
+                break 'out;
+            }
+        }
+    }
+
+    let mut loop_count = 0;
+    for j in 0..map.len() {
+        for i in 0..map[j].len() {
+            if map[j][i].0 == b'.' {
+                map[j][i].0 = b'O';
+            } else {
+                continue;
+            }
+
+            let mut guard = Bot::new(start.0 as i32, start.1 as i32, 0, 1);
+            loop {
+                match test_forward2(&map, &guard) {
+                    Obstacle::Exit(e) => {
+                        if !e {
+                            loop_count += 1
+                        }
+                        break;
+                    }
+                    Obstacle::Nothing => {
+                        map[guard.y as usize][guard.x as usize].1 += 1;
+                        guard.move_forward();
+                    }
+                    Obstacle::Object => guard.turn_right(),
+                    _ => {}
+                }
+            }
+            map[j][i].0 = b'.';
+            clean_map(&mut map);
+        }
+    }
+
+    loop_count
+}
+
+fn test_forward2(map: &Vec<Vec<(u8, u8)>>, bot: &Bot) -> Obstacle {
+    let fx = bot.x + bot.dx;
+    let fy = bot.y - bot.dy;
+    if fy < 0 || fx < 0 || fy >= map.len() as i32 || fx >= map[fy as usize].len() as i32 {
+        return Obstacle::Exit(true);
+    }
+    if map[fy as usize][fx as usize].1 > 3 {
+        return Obstacle::Exit(false);
+    }
+
+    if map[fy as usize][fx as usize].0 == b'.' || map[fy as usize][fx as usize].0 == b'^' {
+        return Obstacle::Nothing;
+    }
+    if map[fy as usize][fx as usize].0 == b'#' || map[fy as usize][fx as usize].0 == b'O' {
+        return Obstacle::Object;
+    }
+    Obstacle::Visited
+}
+
+fn clean_map(map: &mut Vec<Vec<(u8, u8)>>) {
+    for j in 0..map.len() {
+        for i in 0..map[j].len() {
+            map[j][i].1 = 0;
+        }
+    }
 }
